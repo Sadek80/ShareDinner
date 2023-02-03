62088abbe591ffa6a3a24477e98237122423cc94
Sadek80 <mohamedsadk80@gmail.com>
2023-02-03 17:19:07 +0200

    Adds API Dependencies

---
+++ BuberDinner.API/DependencyInjection/APIDependencies.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Reflection;
+
+namespace BuberDinner.API.DependencyInjection
+{
+    public static class APIDependencies
+    {
+        public static IServiceCollection AddAPIDependencies(this IServiceCollection services)
+        {
+            services.AddControllers().AddNewtonsoftJson(options =>
+            {
+                options.SerializerSettings.ContractResolver =
+                                             new DefaultContractResolver();
+                options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+            });
+
+            services.AddEndpointsApiExplorer();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "BubberDinner.API", Version = "v1" });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+
+                // Enables Swagger Documentation
+                c.IncludeXmlComments(xmlPath);
+            });
+
+            services.AddCors();
+
+            services.AddHttpContextAccessor();
+
+            services.AddApiVersioning(config =>
+            {
+                // Specify the default API Version
+                config.DefaultApiVersion = new ApiVersion(1, 0);
+                // If the client hasn't specified the API version in the request, use the default API version number 
+                config.AssumeDefaultVersionWhenUnspecified = true;
+                // Advertise the API versions supported for the particular endpoint
+                config.ReportApiVersions = true;
+            });
+
+            return services;
+        }
+    }
+}
--- BuberDinner.API/Program.cs
+++ BuberDinner.API/Program.cs
@@ -1,52 +1,11 @@
+using BuberDinner.API.DependencyInjection;
 using BuberDinner.Application.DependencyInjection;
 using BuberDinner.Infrastructure.DependencyInjection;
-using Microsoft.OpenApi.Models;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
-using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-#region Main Dependencies
-builder.Services.AddControllers().AddNewtonsoftJson(options =>
-{
-    options.SerializerSettings.ContractResolver =
-                                 new DefaultContractResolver();
-    options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
-});
-
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("1.0", new OpenApiInfo { Title = "BubberDinner.API", Version = "1.0" });
-
-    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-
-    // Enables Swagger Documentation
-    c.IncludeXmlComments(xmlPath);
-
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Name = "Authorization",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer",
-        BearerFormat = "JWT",
-        Description = "Input your Bearer token in this format - Bearer {your token here} to access this API",
-    });
-
-});
-
-builder.Services.AddCors();
-
-builder.Services.AddHttpContextAccessor();
-#endregion
-
-builder.Services.AddApplication()
+builder.Services.AddAPIDependencies()
+                .AddApplication()
                 .AddInfrastructure();
 
 var app = builder.Build();
