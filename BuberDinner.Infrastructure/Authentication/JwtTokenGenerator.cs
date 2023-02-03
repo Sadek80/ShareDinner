0ff7736ae5f4cbef327ddd7b2af479086267ff19
Sadek80 <mohamedsadk80@gmail.com>
2023-02-03 17:40:38 +0200

    Adds JwtTokenGenerator

---
+++ BuberDinner.Infrastructure/Authentication/JwtTokenGenerator.cs
@@ -0,0 +1,33 @@
+﻿using BuberDinner.Application.Common.Interfaces.Authentication;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace BuberDinner.Infrastructure.Authentication
+{
+    public class JwtTokenGenerator : IJwyTokenGenerator
+    {
+        public string GenerateToken(Guid userId, string firstName, string lastName)
+        {
+            var signingCredintials = new SigningCredentials(
+                new SymmetricSecurityKey(Encoding.UTF8.GetBytes("super-secret-key")),
+                SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, userId.ToString()),
+                new Claim(JwtRegisteredClaimNames.GivenName, firstName),
+                new Claim(JwtRegisteredClaimNames.FamilyName, lastName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            };
+
+            var securityToken = new JwtSecurityToken(issuer: "SharedDinner", 
+                                                     expires: DateTime.Now.AddDays(1), 
+                                                     claims: claims,
+                                                     signingCredentials: signingCredintials);
+
+            return new JwtSecurityTokenHandler().WriteToken(securityToken);
+        }
+    }
+}
