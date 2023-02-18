cdd22000ba8e04ab95ef6194bd6bcaf4d6f6cc4a
Sadek80 <mohamedsadk80@gmail.com>
2023-02-18 14:09:07 +0200

    Adds LoginQueryValidator

---
+++ BuberDinner.Application/Services/Authentication/Queries/Login/LoginQueryValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace BuberDinner.Application.Services.Authentication.Queries.Login
+{
+    public class LoginQueryValidator : AbstractValidator<LoginQuery>
+    {
+        public LoginQueryValidator()
+        {
+            RuleFor(x => x.Email).EmailAddress();
+            RuleFor(x => x.Password).MinimumLength(8);
+        }
+    }
+}
