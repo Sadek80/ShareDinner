0edaa7ff270067aae9c5e4aabaaff8fc9ebe8b09
Sadek80 <mohamedsadk80@gmail.com>
2023-02-18 13:37:55 +0200

    Adds Generic ValidationPipelineBehavior

---
+++ BuberDinner.Application/Common/Interfaces/ValidationBehaviors/ValidationPipelineBehavior.cs
@@ -0,0 +1,42 @@
+﻿using ErrorOr;
+using FluentValidation;
+using Mediator;
+
+namespace BuberDinner.Application.Common.Interfaces.ValidationBehaviors
+{
+    public class ValidationPipelineBehavior<TRequest, TResponse> :
+                 IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+                                                        where TResponse : IErrorOr
+    {
+        private readonly IValidator<TRequest>? _validator;
+
+        public ValidationPipelineBehavior(IValidator<TRequest>? validator = null)
+        {
+            _validator = validator;
+        }
+
+        public async ValueTask<TResponse> Handle(TRequest message,
+                                                 CancellationToken cancellationToken,
+                                                 MessageHandlerDelegate<TRequest, TResponse> next)
+        {
+            if (_validator is null)
+            {
+                return await next(message, cancellationToken);
+            }
+
+            var validationResult = await _validator.ValidateAsync(message, cancellationToken);
+
+            if (validationResult.IsValid)
+            {
+                return await next(message, cancellationToken);
+            }
+
+            var errors = validationResult.Errors
+                                         .ConvertAll(validationFailure =>
+                                                     Error.Validation(validationFailure.PropertyName,
+                                                                      validationFailure.ErrorMessage));
+
+            return (dynamic)errors;
+        }
+    }
+}
--- BuberDinner.Application/DependencyInjection/ApplicationDependencies.cs
+++ BuberDinner.Application/DependencyInjection/ApplicationDependencies.cs
@@ -1,4 +1,11 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using BuberDinner.Application.Common.Interfaces.ValidationBehaviors;
+using BuberDinner.Application.Services.Authentication.Commands.Register;
+using BuberDinner.Contracts.Authentication;
+using ErrorOr;
+using FluentValidation;
+using Mediator;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace BuberDinner.Application.DependencyInjection
 {
@@ -7,6 +14,11 @@ namespace BuberDinner.Application.DependencyInjection
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
             services.AddMediator();
+
+            services.AddSingleton(typeof(IPipelineBehavior<,>),
+                                  typeof(ValidationPipelineBehavior<,>));
+
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
 
             return services;
         }
